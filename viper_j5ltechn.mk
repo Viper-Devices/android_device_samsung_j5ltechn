9826b558611694aa3180b2740274be7aabc5b771
EsromTech <esromgalaxy5@gmail.com>
2019-01-23 17:39:43 -0200

    BringUp ViperOS

--- viper_j5ltechn.mk
+++ viper_j5ltechn.mk
@@ -1,12 +1,12 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/j5-common/rr.mk)
+$(call inherit-product, device/samsung/j5-common/viper.mk)
 
 $(call inherit-product, device/samsung/j5ltechn/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5ltechn
-PRODUCT_NAME := rr_j5ltechn
+PRODUCT_NAME := viper_j5ltechn
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J5008
 PRODUCT_MANUFACTURER := samsung
